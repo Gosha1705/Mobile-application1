1486406d8103e1864c72f1c449f190a39dc1ef2a
Gosha1705 <79758776+Gosha1705@users.noreply.github.com>
2024-05-02 22:47:13 +0200

    Delete main.dart

--- main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:portfolio4/Portfolio4/Moive.dart';
-import 'package:portfolio4/Portfolio4/MScreen.dart';
-void main() {
-  runApp(MainScreen());
-}
